8cec3491b4084196c1c87ff2ef70fbd14b416b54
Jeffrey Law <jylday56@gmail.com>
2022-04-03 17:00:17 -0700

    Remove redundant app/ folder role

--- roles/app/files/start-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export DEV_ENV="postgresql://postgres:P%40ssw0rd@localhost:5432/job-tracker-db"
-export SECRET="GREENBEAR"
-
-/backend/.local/bin/gunicorn app:app --bind 0.0.0.0:8080 --chdir /backend/api
